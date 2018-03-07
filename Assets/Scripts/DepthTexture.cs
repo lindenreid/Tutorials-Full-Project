cab46dd853831fb837bceea479f7f5c5f36f7ac3
Lindsey Reid <thelindseyreid@gmail.com>
2018-03-07 17:01:57 -0600

    fixed depth texture script

--- Assets/Scripts/DepthTexture.cs
+++ Assets/Scripts/DepthTexture.cs
@@ -4,14 +4,9 @@
 public class DepthTexture : MonoBehaviour {
 
    private Camera cam;
-   public Material material;
 
    void Start () {
       cam = GetComponent<Camera>(); 
       cam.depthTextureMode = DepthTextureMode.Depth;
-   }
-
-   void OnRenderImage (RenderTexture source, RenderTexture destination){
-      Graphics.Blit(source, destination, material);
    }
 }
